967ee8084d900006a3cff8a06938a5fd2a5624dd
romanus93 <obj.romanus@gmail.com>
2021-06-04 08:11:00 +0900

    named-slots

--- v-slot/src/App.vue
+++ v-slot/src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <AppUserList>
+    <template #secondrow="{ item }">
+      <a :href="'tel:' + item.phone"> {{ item.phone }}</a>
+      <a :href="'mailto:' + item.email"> {{ item.email }}</a>
+    </template>
+    <template #loading>
+      <AppSpinner />
+    </template>
+  </AppUserList>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import AppUserList from "./components/AppUserList.vue";
+import AppSpinner from "./components/AppSpinner.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    AppUserList,
+    AppSpinner,
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
